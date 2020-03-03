81463d1f21e00fa130b7030a554e44fa5a570e50
DavisVaughan <davis@rstudio.com>
2020-03-03 08:33:11 -0500

    No need to special case the internal r file

--- src/internal.h
+++ src/internal.h
@@ -2,11 +2,10 @@
 #define RRAY_INTERNAL_H
 
 
-#include "zzz-internal-r.h"
-
 #include "zzz-internal-attrs.h"
 #include "zzz-internal-export.h"
 #include "zzz-internal-node.h"
+#include "zzz-internal-r.h"
 #include "zzz-internal-sexp.h"
 #include "zzz-internal-sym.h"
 
