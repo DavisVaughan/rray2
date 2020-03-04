964191bebaa00f54b4630cd1bd942e57dc3702a2
DavisVaughan <davis@rstudio.com>
2020-03-03 23:50:01 -0500

    Add labels to callables

--- src/init.c
+++ src/init.c
@@ -4,18 +4,32 @@
 // -----------------------------------------------------------------------------
 
 static const r_callable r_callables[] = {
+  // arithmetic.c
   {"export_rray_add",                      (r_fn_ptr) &export_rray_add, 2},
+
+  // broadcast.c
   {"export_rray_broadcast",                (r_fn_ptr) &export_rray_broadcast, 2},
+
+  // dimensionality.c
   {"export_rray_dimensionality",           (r_fn_ptr) &export_rray_dimensionality, 1},
+
+  // dimensions.c
   {"export_rray_dims",                     (r_fn_ptr) &export_rray_dims, 1},
   {"export_rray_dims2",                    (r_fn_ptr) &export_rray_dims2, 2},
   {"export_rray_dims_common",              (r_fn_ptr) &export_rray_dims_common, 3},
   {"export_rray_dims_expand",              (r_fn_ptr) &export_rray_dims_expand, 2},
+
+  // elements.c
   {"export_rray_elements",                 (r_fn_ptr) &export_rray_elements, 1},
   {"export_rray_elements_from_dims",       (r_fn_ptr) &export_rray_elements_from_dims, 1},
+
+  // library.c
   {"export_rray_init_library",             (r_fn_ptr) &export_rray_init_library, 0},
+
+  // strides.c
   {"export_rray_strides",                  (r_fn_ptr) &export_rray_strides, 1},
   {"export_rray_strides_from_dims",        (r_fn_ptr) &export_rray_strides_from_dims, 1},
+
   {NULL, NULL, 0}
 };
 
